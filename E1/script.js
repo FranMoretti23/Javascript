b0adaab5dffe86ceefcd7413f40655c28cc6a85f
FranMoretti23 <Francomoretti96@gmail.com>
2022-09-13 00:48:21 -0300

    first commit

--- E1/script.js
+++ E1/script.js
@@ -42,39 +42,39 @@ let pizzas = [
 
     },
 ]
-/* A) */
+/* A) Imprimir en consola todos las pizzas con id impar*/
 
-/* let idimpares = pizzas.filter(pizzas => pizzas.id % 2-1 == 0)
+let idimpares = pizzas.filter(pizzas => pizzas.id % 2-1 == 0)
  idimpares.forEach((pizzas) => {
    console.log(`La pizza de ${pizzas.nombre} tiene id impar`)
- }); */
+ });
 
 
- /* C)  */
+ /* C) El nombre de cada pizza con su respectivo precio. */
 
-/*  const pizzasconprecio = pizzas.filter(
+ const pizzasconprecio = pizzas.filter(
   (pizzas) => pizzas.nombre + pizzas.precio
 )
 pizzasconprecio.forEach((pizzas) => {
-  console.log(`La pizza de ${pizzas.nombre} vale $${pizzas.precio} `)
-}); */
+  console.log(`La pizza de ${pizzas.nombre} cuesta $${pizzas.precio} `)
+});
 
-/* D) */
+/* D) Todos los ingredientes de cada pizza*/
 
-/* const pizzasconingredientes = pizzas.filter(
+const pizzasconingredientes = pizzas.filter(
   (pizzas) => pizzas.nombre + pizzas.ingredientes
 )
 pizzasconingredientes.forEach((pizzas) => {
-  console.log(`La pizza de ${pizzas.nombre} contiene ${pizzas.ingredientes} `)
-}); */
+  console.log(`La pizza de ${pizzas.nombre} contiene los siguientes ingredientes ${pizzas.ingredientes} `)
+});
 
-/* B) */
+/* B) ¿Hay alguna pizza que valga menos de $600?*/
 
-/* const elpreciomenorA = (pizzas, cantidad) => pizzas.precio > cantidad;
+const elpreciomenorA = (pizzas, cantidad) => pizzas.precio > cantidad;
 
  const haypizzasconmenorprecioa = (precio) => {
    pizzas.some( pizzas => elpreciomenorA(pizzas, precio))
    ? console.log(`Hay pizzas con precio menor a $${precio}`)
    : console.log(`No hay pizzas con precio menor a $${precio}`)
  }
- haypizzasconmenorprecioa(600); */
+ haypizzasconmenorprecioa(600);
